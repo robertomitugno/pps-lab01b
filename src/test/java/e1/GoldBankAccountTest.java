1dcdac3f8f5509f892e97288e380fb90ce09ff75
RobertoMitugno <roberto.mitugno@studio.unibo.it>
2025-03-03 11:45:45 +0100

    test: add testCanDeposit for GoldBankAccount

--- src/test/java/e1/GoldBankAccountTest.java
+++ src/test/java/e1/GoldBankAccountTest.java
@@ -16,4 +16,11 @@ public class GoldBankAccountTest {
         account = new GoldBankAccount(coreBankAccount);
         assertEquals(BALANCE_INITIALLY, account.getBalance());
     }
+
+    @Test
+    public void testCanDeposit() {
+        account = new GoldBankAccount(coreBankAccount);
+        this.account.deposit(500);
+        assertEquals(500, this.account.getBalance());
+    }
 }
