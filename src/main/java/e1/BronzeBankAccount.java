50069c304e59233ace8683b20ac2bc66c0df4ecf
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2025-03-06 14:42:23 +0100

    refactor: add messagge in IllegalStateException

--- src/main/java/e1/BronzeBankAccount.java
+++ src/main/java/e1/BronzeBankAccount.java
@@ -9,7 +9,7 @@ public class BronzeBankAccount extends CoreBankAccount{
     @Override
     public void withdraw(int amount) {
         if (this.getBalance() < amount){
-            throw new IllegalStateException();
+            throw new IllegalStateException("The balance cannot go below 0");
         }
         bankAccount.withdraw(amount);
     }
--- src/main/java/e1/SilverBankAccount.java
+++ src/main/java/e1/SilverBankAccount.java
@@ -10,7 +10,7 @@ public class SilverBankAccount extends CoreBankAccount{
 
     public void withdraw(int amount) {
         if (this.getBalance() < amount){
-            throw new IllegalStateException();
+            throw new IllegalStateException("The balance cannot go below 0");
         }
         bankAccount.withdraw(amount + FEE);
     }
